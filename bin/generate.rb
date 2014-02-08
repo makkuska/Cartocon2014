7844fe4a67220353db3ce0d00c2f4a04de63c038
Jakub Kalab <jakubkalab@gmail.com>
2014-02-08 21:04:53 +0100

    added logos, background and stripe added fonts

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 require_relative '../lib/qr-codes'
 
-csvfile = ARGV[0]
-pdffile = ARGV[1]
-fontfile = ARGV[2] || "#{Dir.pwd}/Ubuntu-R.ttf"
+csv   = ARGV[0]
+pdf   = ARGV[1] || "#{Dir.pwd}/qr.pdf"
+logo  = ARGV[2] || "#{Dir.pwd}/logo.png"
+logo2 = ARGV[3] || "#{Dir.pwd}/logo2.png"
+bg    = ARGV[4] || "#{Dir.pwd}/background.png"
+# font  = ARGV[5] || "#{Dir.pwd}/Ubuntu-R.ttf"
+font  = ARGV[5] || "#{Dir.pwd}/MyriadPro-Regular.ttf"
 
-unless pdffile && csvfile
-  puts 'usage: generate.rb csvfile pdffile'
+unless pdf && csv
+  puts 'usage: generate.rb csvfile [pdffile logo background font]'
   exit(1)
 end
 
-Document.from_csv(csvfile).generate(pdffile,fontfile)
+Document.from_csv(csv,font,logo,logo2,bg).generate(pdf)
--- lib/qr-codes/document.rb
+++ lib/qr-codes/document.rb
@@ -4,16 +4,16 @@ require_relative 'people'
 require_relative 'qr-file'
 
 class Document
-  def initialize(people)
-    @people = people
+  def initialize(people, font, logo, logo2, background)
+    @people, @font, @logo, @logo2, @background = people, font, logo, logo2, background
   end
 
-  def self.from_csv(file)
-    new(People.from_csv(file))
+  def self.from_csv(file, *args)
+    new(People.from_csv(file), *args)
   end
 
-  def generate(pdffile,fontfile)
-    render(fontfile)
+  def generate(pdffile)
+    render
     document.render_file(pdffile)
   end
 
@@ -22,38 +22,58 @@ class Document
   def document
     @document ||= Prawn::Document.new(page_size: "A4")
   end
+  alias_method :d, :document
 
-  def render(fontfile)
-    document.font(fontfile) do
+  def render
+    d.font(@font) do
       @people.each_slice(4) do |(p1,p2,p3,p4)|
         box(p1,0,0) if p1; box(p2,0,1) if p2
         box(p3,1,0) if p3; box(p4,1,1) if p4
-        document.start_new_page if p4
+        d.start_new_page if p4
       end
     end
   end
 
-  HEIGHT = 14.cm
-  WIDTH = 9.2.cm
+  HEIGHT = 12.5.cm
+  WIDTH = 8.2.cm
 
   def box(person,row,col)
     top_left = [col * WIDTH, (1 + invert(row)) * HEIGHT]
-    document.bounding_box(top_left, width: WIDTH, height: HEIGHT) do
-      document.stroke_bounds
-      # image logo
-      p person.name
-      document.text_box person.name, align: :center, size: 18,
-        at: [1.cm,6.5.cm], width: WIDTH - 2.cm
-      document.text_box person.organization, align: :center, size: 14,
-        at: [1.cm,5.5.cm], width: WIDTH - 2.cm
-      document.text_box person.city, align: :center, size: 14,
-        at: [1.cm,4.8.cm], width: WIDTH - 2.cm
-      document.text_box person.topic, align: :center, size: 12,
-        at: [0.5.cm,3.2.cm], width: WIDTH - QrCode::WIDTH - 1.cm,
-        height: QrCode::HEIGHT + 10
-      document.image QrFile.new(person.data).file.path,
-        at: [WIDTH - QrCode::WIDTH - 10, QrCode::HEIGHT + 10]
+    d.bounding_box(top_left, width: WIDTH, height: HEIGHT) do
+      d.stroke_bounds
+      images
+      texts(person)
+      qr_code(person)
     end
+    print '.'
+  end
+
+  def images
+    d.image @background
+    d.image @logo, at: [0,HEIGHT], width: WIDTH
+    d.image @logo2, at: [0,HEIGHT-2.7.cm], width: WIDTH
+    d.fill_color "0000ff"
+    d.fill_rectangle [0,HEIGHT], WIDTH/3, HEIGHT
+  end
+
+  def texts(person)
+    d.fill_color [100, 65, 25, 0]
+    d.text_box person.name, align: :center, size: 18,
+      at: [1.cm,6.5.cm], width: WIDTH - 2.cm
+    d.text_box person.organization, align: :center, size: 14,
+      at: [1.cm,5.5.cm], width: WIDTH - 2.cm
+    d.text_box person.city, align: :center, size: 14,
+      at: [1.cm,4.8.cm], width: WIDTH - 2.cm
+
+    d.fill_color [0, 0, 0, 67]
+    d.text_box person.topic, align: :center, size: 12,
+      at: [0.5.cm,3.5.cm], width: WIDTH - QrCode::WIDTH - 1.cm,
+      height: QrCode::HEIGHT + 10
+  end
+
+  def qr_code(person)
+    d.image QrFile.new(person.data).file.path,
+      at: [WIDTH - QrCode::WIDTH - 10, QrCode::HEIGHT + 10]
   end
 
   def invert(zero_or_one)
