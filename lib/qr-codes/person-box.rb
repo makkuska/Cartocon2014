82005c18963f69f5ab3ec3fef79d2e34a49ed8b7
Jakub Kalab <jakubkalab@gmail.com>
2014-02-09 14:19:02 +0100

    added new transparent logos and background moved objects added horizontal stripe border lines are thinner

--- lib/qr-codes/person-box.rb
+++ lib/qr-codes/person-box.rb
@@ -13,6 +13,7 @@ class PersonBox
 
   def draw(row,col)
     d.bounding_box(top_left(row,col), width: WIDTH, height: HEIGHT) do
+      d.line_width = 0.25
       d.stroke_bounds
       images
       texts
@@ -33,31 +34,32 @@ class PersonBox
   end
 
   def images
-    d.image document.background
-    d.image document.logo, at: [0,HEIGHT], width: WIDTH
-    d.image document.logo2, at: [0,HEIGHT-2.7.cm], width: WIDTH
     d.fill_color "0000ff"
-    d.fill_rectangle [0,HEIGHT], WIDTH/3, HEIGHT
+    d.fill_rectangle [0,1.2.cm], WIDTH, 1.cm
+
+    d.image document.background, at: [0,HEIGHT], width: WIDTH
+    d.image document.logo, at: [0,HEIGHT], width: WIDTH
+    d.image document.logo2, at: [0 + 1.cm,HEIGHT-2.cm], width: WIDTH - 2.cm
   end
 
   def texts
     d.fill_color [100, 65, 25, 0]
     d.text_box person.name, align: :center, size: 18,
-      at: [1.cm,6.5.cm], width: WIDTH - 2.cm
+      at: [1.cm,8.cm], width: WIDTH - 2.cm
     d.text_box person.organization, align: :center, size: 14,
-      at: [1.cm,5.5.cm], width: WIDTH - 2.cm
+      at: [1.cm,7.cm], width: WIDTH - 2.cm
     d.text_box person.city, align: :center, size: 14,
-      at: [1.cm,4.8.cm], width: WIDTH - 2.cm
-
-    d.fill_color [0, 0, 0, 67]
-    d.text_box person.topic, align: :center, size: 12,
-      at: [0.5.cm,3.5.cm], width: WIDTH - QrCode::WIDTH - 1.cm,
-      height: QrCode::HEIGHT + 10
+      at: [1.cm,6.3.cm], width: WIDTH - 2.cm
   end
 
   def qr_code
+    d.fill_color [0, 0, 0, 67]
+    d.text_box person.topic, align: :center, size: 12,
+      at: [0.5.cm,4.5.cm], width: WIDTH - QrCode::WIDTH - 1.cm,
+      height: QrCode::HEIGHT + 10
+
     d.image QrFile.new(person.data).file.path,
-      at: [WIDTH - QrCode::WIDTH - 10, QrCode::HEIGHT + 10]
+      at: [WIDTH - QrCode::WIDTH - 10, QrCode::HEIGHT + 10 + 1.cm]
   end
 
   def invert(zero_or_one)
